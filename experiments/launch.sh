5633a19fa921668ac3d3e827bf6abac2ba7b5f4e
Henrique Silva <henrique.p.silva@outlook.com>
2020-01-10 16:26:18 -0300

    added splitting support

--- experiments/launch.sh
+++ experiments/launch.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 function usage()
 {
-    echo "COMMAND:"
     echo "  $0 [OPTIONS] <EXP_ID> [REPO_DIRECTORY]"
     echo
     echo "  WHERE <EXP_ID> is the identificator of the experiment"
@@ -20,6 +19,11 @@ function usage()
     echo "      use another dir instead of the default $HOME/Installs/spack"
     echo "    -p | --partitions[=]list,of,partitions,comma,separated"
     echo "      define the desired partitions to be used (default: cei)"
+    echo "    -s | --split"
+    echo "      split the execution plan between all nodes that ought to be used"
+    echo "      WARNING: the splitting will occur between same-partition nodes only"
+    echo "               i.e.: if more than one partition is listed, we'll repeat"
+    echo "               the process in the other partitions"
     echo "    -n | --nodes[=]list,of,nodes,comma,separated"
     echo "      define the desired nodes to be used"
     echo "      WARNING: this option disables usage of the partition list!"
@@ -35,7 +39,6 @@ for i in "$@"; do
     case $i in
         -h|--help)
             echo "USAGE:"
-            echo
             usage
             exit
             ;;
@@ -54,6 +57,10 @@ for i in "$@"; do
         -i|--install)
             shift
             INSTALL_DIR=$1
+            shift
+            ;;
+        -s|--split)
+            SPLIT=true
             shift
             ;;
         --partitions=*)
@@ -105,6 +112,9 @@ UPDATE=${UPDATE:-false}
 # local install boolean
 LOCAL=${LOCAL:-false}
 
+# the split plan boolean
+SPLIT=${SPLIT:-false}
+
 if [[ $REPO_DIR != /* ]]; then
     echo "Path to repository is not absolute, please use the absolute path..."
     exit
@@ -128,7 +138,7 @@ pushd $REPO_DIR
 
 for partition in $PARTITIONLIST; do
     # lets install all needed dependencies first
-    echo "Launching dependency installing job for partition $partition!"
+    echo "-> Launching dependency installing job for partition $partition!"
     if [ $DRY = true -a $LOCAL != true ]; then
         echo "sbatch"
         echo "-p $partition"
@@ -144,6 +154,7 @@ for partition in $PARTITIONLIST; do
             -J dependencies_${EXPERIMENT_ID}_${partition} \
             -W \
             $(dirname $EXP_DIR)/deps.sh $INSTALL_DIR $EXP_DIR
+        echo
     fi
     echo "... and done!"
     echo
@@ -156,6 +167,16 @@ for partition in $PARTITIONLIST; do
         nodes=$(grep "$NODELIST" <<<$ALLNODES | paste -s -d" " -)
     fi
 
+    # splits the plan if we were told to
+    if [ $SPLIT = true ]; then
+        num_nodes=$(wc -w <<<$nodes)
+        rm -f $EXP_DIR/runs.plan.*
+        split -n l/$num_nodes -d -a 1 $EXP_DIR/runs.plan $EXP_DIR/runs.plan.
+    fi
+
+    # counter to access the correct plan
+    plan_part=${num_nodes:+0}
+
     for execution in $nodes; do
         # launch the slurm script for this node
         echo "Launching job for node ${execution%%_*}..."
@@ -165,7 +186,7 @@ for partition in $PARTITIONLIST; do
             echo "-w ${execution%%_*}"
             echo "-c ${execution#*_}"
             echo "-J qr_analysis_${EXPERIMENT_ID}"
-            echo "$EXP_DIR/exp.slurm $EXPERIMENT_ID $EXP_DIR $INSTALL_DIR $LOCAL"
+            echo "$EXP_DIR/exp.slurm $EXPERIMENT_ID $EXP_DIR $INSTALL_DIR $LOCAL ${plan_part:-}"
             echo
         else
             sbatch \
@@ -173,9 +194,15 @@ for partition in $PARTITIONLIST; do
                 -w ${execution%%_*} \
                 -c ${execution#*_} \
                 -J qr_analysis_${EXPERIMENT_ID} \
-                $EXP_DIR/exp.slurm $EXPERIMENT_ID $EXP_DIR $INSTALL_DIR $LOCAL
+                $EXP_DIR/exp.slurm $EXPERIMENT_ID $EXP_DIR $INSTALL_DIR $LOCAL ${plan_part:-}
+        fi
+
+        if [ -z ${plan_part+z} ]; then
+            plan_part=$((plan_part+1))
         fi
     done
+
+    echo
 done
 
 popd
